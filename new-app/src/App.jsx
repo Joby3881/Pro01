882918613a801ad945e6c6ce9361fc23e98318ed
Joby3881 <jobyantony3881@gmail.com>
2024-01-30 13:27:46 +0530

    project

---
+++ new-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+ 
+  return (
+    <>
+    <Nav/>
+      
+    </>
+  )
+      
+}
+
+export default App
---
+++ new-app/src/Components/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Nav.css"
+
+function Nav() {
+  return (
+    <div className='navv'></div>
+  )
+}
+
+export default Nav
